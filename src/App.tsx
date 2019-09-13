4255d546dfbcdc56b2f24d431b678e0a33bb33b9
Clayton Grassick <clayton@claytronics.org>
2019-09-13 10:10:52 -0400

    Basic framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState, FC } from 'react';
 import './App.css';
+import { createHashHistory, Location } from 'history';
+import UrlPattern from 'url-pattern'
 
-const App: React.FC = () => {
+const history = createHashHistory()
+
+const App = () => {
+  const [location, setLocation] = useState(history.location)
+
+  useEffect(() => {
+    // Listen for changes to the current location.
+    const unlisten = history.listen((location) => {
+      setLocation(location)
+    })
+    return unlisten
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar location={location}/>
+      <div className="container">
+        <h1>{location.pathname}</h1>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
+
+
+const NavLink: FC<{ pattern: string, url: string, location: Location }> = (props) => {
+  const active = new UrlPattern(props.pattern).match(props.location.pathname)
+
+  return (
+    <li className={ active ? "nav-item active" : "nav-item" }>
+      <a className="nav-link" href={"#" + props.url}>{props.children}</a>
+    </li>
+  )
+}
+
+
+const Navbar = (props: { location: Location }) => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand" tabIndex={1}>Ouranos</a>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <NavLink pattern="/" url="/" location={props.location}>
+              Accueil
+            </NavLink>
+            <NavLink pattern="/" url="/projet" location={props.location}>
+              Le projet
+            </NavLink>
+            <NavLink pattern="/panacees" url="/panacees" location={props.location}>
+              PANACÉES
+            </NavLink>
+            <NavLink pattern="/adaptation" url="/adaptation" location={props.location}>
+              Vers l'adaptation
+            </NavLink>
+            <NavLink pattern="/liens" url="/liens" location={props.location}>
+              Liens utiles
+            </NavLink>
+            <NavLink pattern="/faq" url="/faq" location={props.location}>
+              FAQ
+            </NavLink>
+          </ul>
+        </div>
+      </div>
+    </nav>)
+}
