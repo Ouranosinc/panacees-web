5faf692299980ccd4556f412c422adce1057c42b
Clayton Grassick <clayton@claytronics.org>
2020-10-15 10:47:00 -0400

    Final fixes

--- src/App.tsx
+++ src/App.tsx
@@ -101,10 +101,13 @@ const Navbar = (props: { location: Location }) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#" style={{ width: 90 }}>
-          <img src="ouranos-color.png" alt="Ouranos" style={{ position: "absolute", top: -1, height: 71, left: -1, cursor: "pointer" }} />
-        </a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>        
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <a className="navbar-brand" href="#" style={{ width: 90 }}>
+            <img src="ouranos-color.png" alt="Ouranos" style={{ position: "absolute", top: -1, height: 71, left: -1, cursor: "pointer" }} />
+          </a>
           <ul className="navbar-nav mr-auto">
             <NavLink pattern="/" url="/" location={props.location}>
               Accueil
--- src/MarkdownPage.tsx
+++ src/MarkdownPage.tsx
@@ -2,6 +2,27 @@ import { useEffect, useState } from "react";
 import Markdown from 'markdown-it'
 import React from "react";
 
+const markdown = new Markdown({ html: true })
+
+// Remember old renderer, if overridden, or proxy to default renderer
+var defaultRender = markdown.renderer.rules.link_open || function(tokens, idx, options, env, self) {
+  return self.renderToken(tokens, idx, options);
+}
+
+markdown.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+  // If you are sure other plugins can't add `target` - drop check below
+  var aIndex = tokens[idx].attrIndex('target');
+
+  if (aIndex < 0) {
+    tokens[idx].attrPush(['target', '_blank']); // add new attribute
+  } else {
+    tokens[idx].attrs[aIndex][1] = '_blank';    // replace value of existing attr
+  }
+
+  // pass token to default renderer.
+  return defaultRender(tokens, idx, options, env, self);
+};
+
 /** Simple markdown page */
 const MarkdownPage = (props: { path: string }) => {
   const [md, setMd] = useState<string>()
@@ -18,7 +39,7 @@ const MarkdownPage = (props: { path: string }) => {
 
   return <div 
     style={{ textAlign: "center", paddingTop: 20 }}
-    dangerouslySetInnerHTML={{ __html: new Markdown({ html: true }).render(md) }}
+    dangerouslySetInnerHTML={{ __html: markdown.render(md) }}
     />
 }
 
